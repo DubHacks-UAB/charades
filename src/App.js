e6569b310990bad7192a2696244b93c81b8b46b7
mahin doshi <mahindoshi@mahins-MacBook-Pro.local>
2020-10-17 18:24:41 -0600

    hard coded choices for charades

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import VideoKitComponent from './VideoKitComponent';
 
 function App() {
+  
+  var movieList = ["The Gentlemen", "Bad boys", "The Godfather",
+                  "Mulan", "The Avengers", "Inception", "Joker",
+                 "The Incredibles", "Frozen", "The Lion King", 
+                 "Secret Life Of Pets", "Little Women", "Black Panther", 
+                  "Parasite", "Mission Impossible", "The Notebook", "Aquaman", "Fury",
+                   "Crazy Rich Asians",  "Pulp Fiction", "Revanent", "The Irishman"];
+
+  var tvShows = ["Friends", "Modern Family", "Suits", "Community", "How I Met Your Mother",
+                "Normal People", "The Witcher", "Fresh Prince of Bel Air", "Peaky Blinders",
+                "The Office", "The Good Place", "Silicon Valley", "The Crown", "Homeland", "Mad Med",
+                "Brooklyn Nine-Nine", "The Last Dance", "New Girl", "Gossip Girl", "The Man In The High Castle",
+                 "The Simpsons", "Game Of Thrones", "Westworld"];
+
+  
   return (
     <div className="App">
       <header className="App-header">
